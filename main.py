b1cd2b4ff2c074bac8ad52a18b69aa6b6d76dc20
victorsiajunza <logzee26@gmail.com>
2022-06-03 01:05:46 +1000

    first atemp to push to github

---
+++ main.py
@@ -0,0 +1,17 @@
+# Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+
+
+with open('story.txt','r') as openfile:
+    print('this file is true')
+
+  
+
+
+def count_words():
+    text = read_file_content("./story.txt")
+    # [assignment] Add your code here
+
+    return {"as": 10, "would": 20}
